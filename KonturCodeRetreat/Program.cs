2203fe5185715b986761538c7158938c6889f119
Gallyam Biktahsev <gallyamb@gmail.com>
2017-02-18 20:25:17 +0500

    Add 'Gliders factory' initial state

--- KonturCodeRetreat/Program.cs
+++ KonturCodeRetreat/Program.cs
@@ -17,8 +17,8 @@ namespace KonturCodeRetreat
         public State[][] RealFiled { get; set; }
         public IEnumerable<Point> GetNeighbours(Point current)
         {
-            var dxs = new[] {-1, 0, 1};
-            var dys = new[] {-1, 0, 1};
+            var dxs = new[] { -1, 0, 1 };
+            var dys = new[] { -1, 0, 1 };
             return dxs.SelectMany(dx => dys, (dx, dy) => new Point(current.X + dx, current.Y + dy)).Where(p => IsInField(p) && !p.Equals(current));
         }
 
@@ -74,6 +74,12 @@ namespace KonturCodeRetreat
             get { return RealFiled[position.X][position.Y]; }
             set { RealFiled[position.X][position.Y] = value; }
         }
+
+        public State this[int x, int y]
+        {
+            get { return this[new Point(x, y)]; }
+            set { this[new Point(x, y)] = value; }
+        }
     }
 
     public enum State
@@ -86,36 +92,77 @@ namespace KonturCodeRetreat
     {
         static void Main(string[] args)
         {
-            var initialState = new State[][]
+            var initialState = Enumerable.Range(0, 20)
+                .Select(_ => Enumerable.Range(0, 40)
+                    .Select(__ => State.Dead)
+                    .ToArray())
+                .ToArray();
+
+            var field = new Field(initialState);
+
+            var alivePoints = new HashSet<Point>
             {
-                new State[] {0, 0 ,0 ,0},
-                new State[] {0, (State) 1 , 0, 0},
-                new State[] {0, (State)1, 0, 0},
-                new State[] {0, (State)1, 0 ,0},
-                new State[] {0, 0 ,0 ,0},
+                new Point(5, 1),
+                new Point(6, 1),
+                new Point(5, 2),
+                new Point(6, 2),
+                new Point(5, 11),
+                new Point(6, 11),
+                new Point(7, 11),
+                new Point(4, 12),
+                new Point(8, 12),
+                new Point(3, 13),
+                new Point(9, 13),
+                new Point(3, 14),
+                new Point(9, 14),
+                new Point(6, 15),
+                new Point(4, 16),
+                new Point(8, 16),
+                new Point(5, 17),
+                new Point(6, 17),
+                new Point(7, 17),
+                new Point(6, 18),
+                new Point(3, 21),
+                new Point(4, 21),
+                new Point(5, 21),
+                new Point(3, 22),
+                new Point(4, 22),
+                new Point(5, 22),
+                new Point(2, 23),
+                new Point(6, 23),
+                new Point(1, 25),
+                new Point(2, 25),
+                new Point(6, 25),
+                new Point(7, 25),
+                new Point(4, 35),
+                new Point(5, 35),
+                new Point(4, 36),
+                new Point(5, 36)
             };
-
-            var filed = new Field(initialState);
-
+            foreach (var alivePoint in alivePoints)
+            {
+                field[alivePoint] = State.Alive;
+            }
 
             for (int i = 0; i < 10000; i++)
             {
-                Visualise(filed.RealFiled);
-                Thread.Sleep(1000);
-                filed.Tick();
+                Visualize(field.RealFiled);
+                Thread.Sleep(100);
+                field.Tick();
             }
         }
 
-        public static void Visualise(State[][] state)
+        public static void Visualize(State[][] state)
         {
             Console.Clear();
             foreach (var row in state)
             {
+                var sb = new StringBuilder();
                 foreach (var item in row)
                 {
-                    Console.Write(item == State.Alive? "■" : " ");
+                    sb.Append(item == State.Alive ? "■" : " ");
                 }
-                Console.WriteLine();
+                Console.WriteLine(sb.ToString());
             }
         }
     }
