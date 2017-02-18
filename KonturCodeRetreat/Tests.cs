6d3a10df2fe09f4c56e717e88b1dbfa39512149d
Gallyam Biktahsev <gallyamb@gmail.com>
2017-02-18 14:48:29 +0500

    1

---
+++ KonturCodeRetreat/Tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace KonturCodeRetreat
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void singleCitizenShoudlDie()
+        {
+            var field = new int[][]
+            {
+                new[] {0}
+            };
+
+            field = GameHandler.makeTurn(field);
+
+            Assert.AreEqual(0, field[0][0]);
+        }
+    }
+}
